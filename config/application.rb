ba4ae0f608de33707b6cb865777e2f1c0e6781d1
Lucas <lclucasgr@gmail.com>
2018-06-22 17:10:59 -0300

    add configuration

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,21 @@
-development:
-  GMAIL_USERNAME: 'lclucas2019@gmail.com'
-  GMAIL_PASSWORD: 'palmeiras2019'
-production:
-  GMAIL_USERNAME: 'lclucas2019@gmail.com'
-  GMAIL_PASSWORD: 'palmeiras2019'
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module FinalProjectDw5
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Brasilia'
+    
+    ENV.update YAML.load_file('config/application.yml')[Rails.env] rescue {}
+  end
+end
