dc5386554ebb9af84f2009096426c7953bcd4185
Lucas <lclucasgr@gmail.com>
2018-06-28 16:40:05 -0300

    change test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,31 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
+
+  def is_admin_logged_in?
+    !session[:admin_id].nil?
+  end
+
+  def log_in_as_admin(admin)
+    session[:admin_id] = admin.id
+  end
+
+end
+  class ActionDispatch::IntegrationTest
+
+ 
+  def log_in_as_admin(admin, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                           } }
+  end
 end
