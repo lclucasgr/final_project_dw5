00da5a5f74eda9dc73677459a4e4ebe90414421c
Lucas <lclucasgr@gmail.com>
2018-07-01 14:20:15 -0300

    add all test model

--- test/models/devolution_test.rb
+++ test/models/devolution_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class DevolutionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @book = books(:porquinhos)
+    # This code is not idiomatically correct.
+    @devolution = Devolution.new(book_id: @book.id)
+  end
+
+  test "should be valid" do
+    assert @devolution.valid?
+  end
+
+  test "book id should be present" do
+    	@devolution.book_id = nil
+    	assert_not @devolution.valid?
+  end 
 end
